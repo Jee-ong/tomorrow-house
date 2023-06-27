a41d032c09465478e5b51d0dea564d827e11c3b9
Jee-ong <wjddlk00k00@gmail.com>
2023-06-27 17:39:50 +0900

    🚧 Initiate a new carousel: .user-gallery

--- js/Carousels.js
+++ js/Carousels.js
@@ -11,3 +11,21 @@ const productCarousel = tns({
   preventScrollOnTouch: true,
   // slideBy: 'page',
 })
+
+const userGallery = tns({
+  container: '.user-gallery .slider-list',
+  gutter: 4,
+  edgePadding: 16,
+  controls: false,
+  loop: false,
+  arrowKeys: true,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+  responsive: {
+    768: {
+      gutter: 6,
+      controls: true,
+      edgePadding: 75,
+    },
+  },
+})
